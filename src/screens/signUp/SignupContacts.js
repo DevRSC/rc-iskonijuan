8e733dc7df63106db3405b54e06d6234d05f5fdf
RamilSC <ramil.cobillajr@tup.edu.ph>
2022-11-30 19:05:35 +0800

    Updated SignupContacts

--- src/screens/signUp/SignupContacts.js
+++ src/screens/signUp/SignupContacts.js
@@ -6,13 +6,26 @@ import {
   Keyboard,
 } from "react-native";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { scale, verticalScale } from "react-native-size-matters";
 import { Text, TextInput } from "react-native-paper";
+import CustomInput from "../../components/CustomInput";
 import LoginButton from "../../components/atoms/LoginButton";
 import { useNavigation } from "@react-navigation/native";
 
 export default function SignupContacts() {
   const navigation = useNavigation();
+
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onRegisterPressed = () => {
+    navigation.navigate("SignUpVerify");
+  };
 
   return (
     <View style={styles.container}>
@@ -36,27 +49,48 @@ export default function SignupContacts() {
               Add your phone number for another layer of security and to receive
               important messages.
             </Text>
-
-            <TextInput
-              name='Phone'
-              mode='outlined'
-              label={"Phone number"}
-              left={<TextInput.Affix text='+639' />}
-              style={styles.input}
-            />
-
-            <Text style={{ color: "#918E9B", marginTop: verticalScale(34) }}>
-              By continuing, you agree to the Isko ni Juan terms, acknowledge
-              receipt of our privacy notice and have read the Fundraising
-              Regulator’s guidance.
-            </Text>
-
-            <LoginButton
-              title='Send Code'
-              style={styles.button}
-              onPress={() => navigation.navigate("#")}
+          </View>
+          <View
+            style={{ height: 100, width: "100%", marginTop: verticalScale(27) }}
+          >
+            <CustomInput
+              name='phone'
+              placeholder='Phone Number'
+              keyboardType='phone-pad'
+              control={control}
+              rules={{
+                required: "Phone Number is required",
+                minLength: {
+                  value: 10,
+                  message: "Phone Number must be 10 digits",
+                },
+                maxLength: {
+                  value: 10,
+                  message: "Phone Number must be 10 digits",
+                },
+              }}
+              props={{
+                keyboardType: "phone-pad",
+                left: (
+                  <TextInput.Affix
+                    text='+63'
+                    textStyle={{ color: "#2B283A" }}
+                  />
+                ),
+              }}
             />
           </View>
+          <Text style={styles.greyText}>
+            By continuing, you agree to the Isko ni Juan terms, acknowledge
+            receipt of our privacy notice and have read the Fundraising
+            Regulator’s guidance.
+          </Text>
+
+          <LoginButton
+            title='Send Code'
+            style={styles.button}
+            onPress={handleSubmit(onRegisterPressed)}
+          />
         </View>
       </TouchableWithoutFeedback>
     </View>
@@ -82,12 +116,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   contactHeader: {
+    width: "100%",
     marginTop: verticalScale(40),
   },
   greyText: {
     color: "#918E9B",
-    fontSize: scale(14),
-    fontWeight: "700",
+    fontSize: scale(13),
+    fontWeight: "600",
     paddingBottom: verticalScale(16),
   },
   HeaderText: {
@@ -96,19 +131,11 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     paddingBottom: verticalScale(16),
   },
-  input: {
-    marginTop: verticalScale(27),
-    width: scale(328),
-    height: verticalScale(55),
-    borderRadius: 8,
-    fontSize: scale(16),
-    fontWeight: "600",
-    color: "#2B283A",
-    marginBottom: verticalScale(16),
-  },
+
   button: {
     marginTop: verticalScale(40),
     borderRadius: 8,
+    width: "100%",
     fontSize: scale(16),
     fontWeight: "600",
   },
