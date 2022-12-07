e2ad400601ae0f8602f608047f4bf055e17d25e6
RamilSC <ramil.cobillajr@tup.edu.ph>
2022-12-08 03:44:42 +0800

    Added CardModal for HomeScreen

---
+++ src/components/molecules/CardModal.js
@@ -0,0 +1,91 @@
+import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
+import Animated, { BounceIn } from "react-native-reanimated";
+import { Text } from "react-native-paper";
+import Fontawesome from "react-native-vector-icons/FontAwesome";
+import { scale, verticalScale } from "react-native-size-matters";
+
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+import LoginButton from "../atoms/LoginButton";
+
+export default function CardModal() {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { Name, Title, Description } = route.params;
+
+  return (
+    //Create Modal Card
+    <SafeAreaView style={styles.container}>
+      <Animated.View entering={BounceIn} style={styles.card}>
+        <View style={styles.cardHeader}>
+          <Text style={styles.cardHeaderText}>{Name}</Text>
+        </View>
+        <View style={styles.cardHeaderLeft}>
+          <Text style={styles.cardSubtitle}>{Title}</Text>
+        </View>
+        <View style={styles.cardBody}>
+          <ScrollView>
+            <Text style={styles.cardBodyText}>{Description}</Text>
+          </ScrollView>
+        </View>
+        <View style={styles.cardFooter}>
+          <LoginButton title='Close' onPress={() => navigation.goBack()} />
+        </View>
+      </Animated.View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  card: {
+    width: "93%",
+    height: verticalScale(420),
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    padding: 20,
+    marginBottom: verticalScale(55),
+  },
+  cardHeader: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardHeaderLeft: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  cardSubtitle: {
+    fontSize: scale(16),
+    fontFamily: "Inter-Medium",
+    color: "#2B283A",
+  },
+
+  cardHeaderText: {
+    fontSize: scale(24),
+    fontFamily: "Inter-SemiBold",
+  },
+  cardBody: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardBodyText: {
+    fontSize: 16,
+    textAlign: "center",
+  },
+  cardFooter: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
