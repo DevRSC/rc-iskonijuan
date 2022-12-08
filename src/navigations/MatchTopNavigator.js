15b75cfa814743d6863a082dc08730b9f36ae79f
RamilSC <ramil.cobillajr@tup.edu.ph>
2022-12-08 17:46:06 +0800

    Update MatchTopNavigator.js

--- src/navigations/MatchTopNavigator.js
+++ src/navigations/MatchTopNavigator.js
@@ -1,18 +1,62 @@
 import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { Dimensions } from "react-native";
+import { scale, verticalScale } from "react-native-size-matters";
 
-import Campaign from "../screens/matchTopScreen/LikedCampaign";
-import Stories from "../screens/matchTopScreen/SuccessStories";
+import Test from "./Test";
+
+import LikedCampaign from "../screens/matchTopScreen/LikedCampaign";
+import SuccessStories from "../screens/matchTopScreen/SuccessStories";
 
 const topTab = createMaterialTopTabNavigator();
 
 const { width } = Dimensions.get("window");
 
+const LikedStack = createNativeStackNavigator();
+const LikedStackScreen = () => {
+  return (
+    <LikedStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <LikedStack.Screen name='LikedStackScreen' component={LikedCampaign} />
+
+      <LikedStack.Screen
+        name='Test'
+        component={Test}
+        options={{
+          title: "Test",
+        }}
+      />
+    </LikedStack.Navigator>
+  );
+};
+
+const SuccessStack = createNativeStackNavigator();
+const SuccessStackScreen = () => {
+  return (
+    <SuccessStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <SuccessStack.Screen
+        name='SuccessStackScreen'
+        component={SuccessStories}
+        options={{
+          title: "Success Stories",
+        }}
+      />
+    </SuccessStack.Navigator>
+  );
+};
+
 export default function MatchTopNavigator() {
   return (
     <topTab.Navigator
-      initialRouteName='Campaign'
+      initialRouteName='Liked Campaign'
       initialLayout={width}
       screenOptions={{
         tabBarLabelStyle: {
@@ -31,8 +75,10 @@ export default function MatchTopNavigator() {
         },
       }}
     >
-      <topTab.Screen name='Liked Campaign' component={Campaign} />
-      <topTab.Screen name='Success Stories' component={Stories} />
+      <topTab.Screen name='Liked Campaign' component={LikedStackScreen} />
+      <topTab.Screen name='Success Stories' component={SuccessStackScreen} />
+      {/* <topTab.Screen name='Liked Campaign' component={Campaign} />
+      <topTab.Screen name='Success Stories' component={Stories} /> */}
     </topTab.Navigator>
   );
 }
