6d2da6326619801a1325327c635a1c29f96e2e16
RamilSC <ramil.cobillajr@tup.edu.ph>
2022-12-08 17:46:12 +0800

    Update HomeScreen.js

--- src/screens/bottomScreens/HomeScreen.js
+++ src/screens/bottomScreens/HomeScreen.js
@@ -22,8 +22,7 @@ const DummyData = [
     campaignName: "For Academic Books",
     camapaignDescription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit vel quam vitae ultrices. Morbi tincidunt risus libero, eu feugiat nisi dictum eget. ",
-    image:
-      "https://scontent.fmnl30-3.fna.fbcdn.net/v/t1.6435-9/96600405_2792639730864865_2489118605287882752_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHUy0CGVvSGrPNROJ_KD1ZRWnkm3ayW9qNaeSbdrJb2o0wCMK1-fFUB6_Vk2Coczgkx3jRe_24FtKttM3vM-r6S&_nc_ohc=RjoqDv_V7mIAX9hXDGt&_nc_oc=AQn_Gkgk-YyHOmxXXtgYZ7ComVya-JHyNcdYwYeW8OFsZQlWfX14T-iQ5q0kSBBTxqU&_nc_ht=scontent.fmnl30-3.fna&oh=00_AfCMTqNyuoXVWIsQP8yVSxA4ik3BNs8L236wgMxiq6k15w&oe=639CDAA3",
+    image: "https://picsum.photos/700",
     donationProgress: 0.5,
     verified: false,
     currentDonation: 1000,
@@ -35,8 +34,7 @@ const DummyData = [
     campaignName: "For Academic Books",
     camapaignDescription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit vel quam vitae ultrices. Morbi tincidunt risus libero, eu feugiat nisi dictum eget. ",
-    image:
-      "https://scontent.fmnl30-2.fna.fbcdn.net/v/t39.30808-6/299904457_5244890578942036_4207457958574787941_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGvisv85fqKRujF4Ol3OIp6z8hjh-GYZHrPyGOH4ZhkemdOMEgBZPhPRMpNL0SewuvwCOmlZGtcEfxwF-YLnrrF&_nc_ohc=qMd7llTqSCYAX8AKyp_&_nc_ht=scontent.fmnl30-2.fna&oh=00_AfDz1Khyb1whiMI96i4nzihxlk952_xZUVNZFk9Wv3BZEA&oe=637ADAE4",
+    image: "https://picsum.photos/700",
     donationProgress: 0.2,
     verified: true,
     currentDonation: 1000,
@@ -48,8 +46,7 @@ const DummyData = [
     campaignName: "For Academic Books",
     camapaignDescription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit vel quam vitae ultrices. Morbi tincidunt risus libero, eu feugiat nisi dictum eget. ",
-    image:
-      "https://scontent.fmnl30-3.fna.fbcdn.net/v/t39.30808-6/241744456_4449126251801368_939455980581216076_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGAB0Lo3KIHXD3rJioBMK8EL0ddhS7XKGUvR12FLtcoZZEqa83vkguDv3pfT2hE-fM-yYeSvjbGsnQA5Cw9p658&_nc_ohc=yxlQF-NvCi8AX8dREH9&tn=QDPAccx4kdUj6ijQ&_nc_ht=scontent.fmnl30-3.fna&oh=00_AfA4aVU_Pg-FOBJ5P19i-hcJ12y2clKfwU5aN-rmLJdlmQ&oe=637B2E8C",
+    image: "https://picsum.photos/700",
     donationProgress: 0.7,
     verified: false,
     currentDonation: 1000,
@@ -61,8 +58,7 @@ const DummyData = [
     campaignName: "For Academic Books",
     camapaignDescription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit vel quam vitae ultrices. Morbi tincidunt risus libero, eu feugiat nisi dictum eget. ",
-    image:
-      "https://scontent.fmnl30-2.fna.fbcdn.net/v/t39.30808-6/274471476_5029773607043904_5501496730304066253_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFVuyyTldKXoe1GqmN6erJZtcLSjHmZibG1wtKMeZmJsbGqinAqS54k7fdah1gmhcSZuy0hn-k_GafQ6w0Th7Mk&_nc_ohc=__zVcothCxcAX-qSNha&_nc_ht=scontent.fmnl30-2.fna&oh=00_AfDaxYC42nLVY8zOHr2eJQmADWbRhqhFf-UYG03wSkiB-Q&oe=6379E77A",
+    image: "https://picsum.photos/700",
     donationProgress: 0.6,
     verified: true,
     currentDonation: 1000,
@@ -74,8 +70,7 @@ const DummyData = [
     campaignName: "For Academic Books",
     camapaignDescription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit vel quam vitae ultrices. Morbi tincidunt risus libero, eu feugiat nisi dictum eget. ",
-    image:
-      "https://scontent.fmnl30-3.fna.fbcdn.net/v/t1.6435-9/124624436_3450456335034781_5069551858126246879_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=174925&_nc_eui2=AeG4tdAih7T7kPKEbfWniDmd7k4y40eW32DuTjLjR5bfYFXUU43lLqaq_BT30oWBOL9mgx_1p4BDEFar1a6vALx9&_nc_ohc=oGYLDponXoYAX9ZsmMY&_nc_ht=scontent.fmnl30-3.fna&oh=00_AfDzIx9CZ3UWv-KnLdqmWCAyEjCroAgzV5Wn6fgMg24dPA&oe=639CCF64",
+    image: "https://picsum.photos/700",
     donationProgress: 0.9,
     verified: true,
     currentDonation: 1000,
