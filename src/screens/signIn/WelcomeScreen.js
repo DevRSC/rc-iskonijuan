7c70f2edbe211fe02d9f143b9491d5b1301ccfe6
RamilSC <ramil.cobillajr@tup.edu.ph>
2022-12-13 01:11:45 +0800

    Update WelcomeScreen.js

--- src/screens/signIn/WelcomeScreen.js
+++ src/screens/signIn/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { StyleSheet, View, Image } from "react-native";
 import WelcomeContainer from "../../components/molecules/WelcomeContainer";
 import Animated, { SlideInDown, BounceIn } from "react-native-reanimated";
