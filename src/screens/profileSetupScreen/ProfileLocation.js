de7d46b667ad290631bef01d6d1f1d2b61a00a65
RamilSC <ramil.cobillajr@tup.edu.ph>
2023-01-04 13:18:14 +0800

    Update ProfileLocation.js

--- src/screens/profileSetupScreen/ProfileLocation.js
+++ src/screens/profileSetupScreen/ProfileLocation.js
@@ -1,23 +1,67 @@
 import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-svg";
-import ProfileSetupHeader from "../../components/molecules/ProfileSetupHeader";
-import React from "react";
+import { Text } from "react-native-paper";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
-
-import LoginButton from "../../components/atoms/LoginButton";
 import { scale, verticalScale } from "react-native-size-matters";
+import * as Location from "expo-location";
+
+import ProfileSetupHeader from "../../components/molecules/ProfileSetupHeader";
+import LoginButton from "../../components/atoms/LoginButton";
 
 export default function ProfileLocation() {
   const navigation = useNavigation();
+  const [sendLocation, setSendLocation] = useState(false);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (sendLocation) {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          setErrorMsg("Permission to access location was denied");
+          return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        setSendLocation(false);
+      }
+    })();
+  }, [sendLocation]);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  //get latitude and longitude from text variable
+  console.log(location?.coords.latitude);
+  console.log(location?.coords.longitude);
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inner}>
         <ProfileSetupHeader number={2} title='Current Location' />
+        <Text style={{ fontSize: 16, color: "#000000", marginTop: 16 }}>
+          We need to know where you are so we can show you the best matches
+          nearby.
+        </Text>
+        <Text style={{ fontSize: 16, color: "red", marginTop: 16 }}>
+          {text}
+        </Text>
+        <View style={{ alignItems: "center", width: "100%", marginTop: 16 }}>
+          <LoginButton
+            title='Use Current Location'
+            onPress={() => setSendLocation(true)}
+          />
+        </View>
       </View>
+
       <View
         style={{
-          marginTop: verticalScale(114),
           flexDirection: "row",
           justifyContent: "space-between",
           paddingHorizontal: scale(16),
