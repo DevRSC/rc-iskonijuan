a7d49ea3e2d8c8d82ec80583135c4a85df9ebf1c
RamilSC <ramil.cobillajr@tup.edu.ph>
2023-01-06 00:31:11 +0800

    Update ProfileEthnicity.js

--- src/screens/profileSetupScreen/ProfileEthnicity.js
+++ src/screens/profileSetupScreen/ProfileEthnicity.js
@@ -1,36 +1,90 @@
 import { StyleSheet, Text, View } from "react-native";
 import ProfileSetupHeader from "../../components/molecules/ProfileSetupHeader";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { scale, verticalScale } from "react-native-size-matters";
 import { useNavigation } from "@react-navigation/native";
 import LoginButton from "../../components/atoms/LoginButton";
+import { SelectList } from "react-native-dropdown-select-list";
+
+const data = [
+  {
+    key: 1,
+    value: "African",
+  },
+  {
+    key: 2,
+    value: "Asian",
+  },
+  {
+    key: 3,
+    value: "European",
+  },
+  {
+    key: 4,
+    value: "Latin American",
+  },
+  {
+    key: 5,
+    value: "Middle Eastern",
+  },
+  {
+    key: 6,
+    value: "Pacific Islander",
+  },
+  {
+    key: 7,
+    value: "Indigenous/Native American",
+  },
+  {
+    key: 8,
+    value: "Multiracial/Mixed-Race",
+  },
+  {
+    key: 9,
+    value: "Other (e.g. Romani, Jewish, etc.)",
+  },
+];
 
 export default function ProfileEthnicity() {
   const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inner}>
         <ProfileSetupHeader number={4} title={"Ethnicity"} />
+        <View style={{ marginTop: 16 }}>
+          <SelectList
+            data={data}
+            setSelected={setSelected}
+            save='value'
+            fontFamily={"Inter-Regular"}
+            inputStyles={{
+              fontSize: scale(14),
+              fontFamily: "Inter-Regular",
+            }}
+          />
+        </View>
       </View>
-      <View
-        style={{
-          marginTop: verticalScale(114),
-          flexDirection: "row",
-          justifyContent: "space-between",
-          paddingHorizontal: scale(16),
-        }}
-      >
-        <LoginButton
-          title='Go Back'
-          onPress={() => navigation.goBack()}
-          style={styles.button}
-        />
-        <LoginButton
-          title='Next'
-          onPress={() => navigation.navigate("ProfileStudy")}
-          style={styles.button}
-        />
+      <View style={styles.footer}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            paddingHorizontal: scale(16),
+          }}
+        >
+          <LoginButton
+            title='Go Back'
+            onPress={() => navigation.goBack()}
+            style={styles.button}
+          />
+          <LoginButton
+            title='Next'
+            onPress={() => navigation.navigate("ProfileStudy")}
+            style={styles.button}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -42,6 +96,13 @@ const styles = StyleSheet.create({
   },
   inner: {
     padding: 16,
-    alignItems: "flex-start",
+  },
+  footer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: verticalScale(36),
+  },
+  button: {
+    borderRadius: 8,
   },
 });
