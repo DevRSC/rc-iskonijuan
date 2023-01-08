cd89080c2ebcf02133785be0fceaab5e348a0a9b
RamilSC <ramil.cobillajr@tup.edu.ph>
2023-01-08 23:53:09 +0800

    Added Amplify dependencies

---
+++ amplify/backend/function/cognitoToDynamoDB/src/index.js
@@ -0,0 +1,46 @@
+var aws = require('aws-sdk')
+var ddb = new aws.DynamoDB()
+
+exports.handler = async (event, context) => {
+    let date = new Date()
+    if (event.request.userAttributes.sub) {
+        let params = {
+            Item: {
+                'id': {S: event.request.userAttributes.sub},
+                'userType': {S: event.request.userAttributes["custom:userType"]},
+                'username': {S: event.userName},
+                'email': {S: event.request.userAttributes.email},
+                'firstName': {S: event.request.userAttributes.given_name},
+                'middleName': {S: event.request.userAttributes["custom:middleName"]},
+                'lastName': {S: event.request.userAttributes.family_name},
+                'sex': {S: event.request.userAttributes["custom:sex"]},
+                'address': {S: event.request.userAttributes["custom:address"]},
+                'postal': {S: event.request.userAttributes["custom:postal"]},
+                'nationality': {S: event.request.userAttributes["custom:nationality"]},
+                'typeOfDisability': {S: event.request.userAttributes["custom:typeOfDisability"]},
+                'placeOfBirth': {S: event.request.userAttributes["custom:placeOfBirth"]},
+                'birthDate': {S: event.request.userAttributes["custom:birthDate"]},
+                'mobileNumber': {S: event.request.userAttributes.phone_number},
+                'createdAt': {S: date.toISOString()},
+                'updatedAt': {S: date.toISOString()},
+                'status': {S: 'null'},
+                'image': {S: 'null'},
+            },
+            TableName: process.env.USERTABLE
+        }
+
+        try {
+            await ddb.putItem(params).promise()
+            console.log("Success")
+        } catch (err) {
+            console.log("Error", err)
+        }
+
+        console.log("Success: Everything executed correctly")
+        context.done(null, event)
+
+    } else {
+        console.log("Error: Nothing was written to DynamoDB")
+        context.done(null, event)
+    }
+};
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "iskonijuanv2": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "cognitoToDynamoDB": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string",
+            "LambdaExecutionRoleArn": "string"
+        }
+    }
+}
